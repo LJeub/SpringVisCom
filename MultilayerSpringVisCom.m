fcb7f962b42f32f8982440c558ecebf0c94ce644
Lucas Jeub <ljeub@iu.edu>
2018-05-08 18:43:51 +0200

    Add documentation and implement missing options

--- MultilayerSpringVisCom.m
+++ MultilayerSpringVisCom.m
@@ -1,5 +1,90 @@
 function xy=MultilayerSpringVisCom(A,S,varargin)
+% [XY]=MULTILAYERSPRINGVISCOM(A,S,OPTIONS)
+%
+% calculate coordinates for visualising multilayer networks with community
+% structure
+%
+% Inputs:
+%
+%   A: cell array of adjacency matrices for each layer
+%
+%   S: community assignment:
+%           possible forms:
+%               group vector matrix: each column gives the community index 
+%                   for each node in a layer
+%                   [size: (#nodes,#layers)]
+%
+%               group matrix array: cell array of group matrices for each 
+%                   layer. Each matrix gives the strength of community 
+%                   memberships for each node to each community, used for 
+%                   overlapping communities
+%                   [size: {(#nodes,#groups)}x#layers]
+%
+%               emtpty: ([]) results in the usual Kamada-Kawai layout
+%                   without community structure
+%
+%
+%   OPTIONS:
+%           epsilon: [0.01] tolerance for termination criterion
+%
+%           community_field: [1] repulsive charge between nodes in different
+%               communities, increase to separate communities more
+%
+%           background_field: [0] repulsive charge between all nodes (useful
+%               in combination with distance_matrix=true, when the distance
+%               matrix is sparse)
+%
+%           dimension: [2] dimension of the layout space
+%
+%           distance_matrix: [false] set to true to treat A as a distance
+%               matrix rather than an adjacency matrix
+%
+%           verbose: [false] set to true to display improvement at each step
+%
+%           optimisation_groups: [S] groups used for optimisation
+%               (group vector or group matrix)
+%
+%           fixed_nodes: [] format [node, x, y,...] keep these nodes fixed
+%               at given coordinates
+%
+%           progressive: [] initial coordinates for all nodes, when
+%               progressive is specified, fixed_nodes can be given as list
+%               of node indices, coordinates specified in fixed_nodes take
+%               precendence over those in progressive, if communities_only
+%               is set, give coordinates for each community
+%
+%           communities_only: [false] set to true to compute only community
+%               coordinates
+%
+%           c1: [0.0001] first constant in Wolfe-conditions for line-search
+%
+%           c2: [0.9] second constant in Wolfe-conditions for line-search
+%
+%   options are specified as a comma-separated list of key-value pairs or
+%   as a struct of options
+%
+% Outputs:
+%
+%   xy: array of coordinates for each physical node [size: (#nodes,dimension)]
+%
+% Requires:
+%
+%   matlab_bgl
+%   spring_system
+%   BFGS_update
+%   wolfe_step
+%   div_0
+%
+% The node layout is computed by minimising an energy function which is
+% based on a spring system (see Tomihisa Kamada and Satoru Kawai:
+% "An Algorithm For Drawing General Undirected Graphs"), where nodes are
+% charged particles, such that nodes in different communities repel each
+% other.
 
+% Version: 1.2
+% Date: Tue 13 May 2014 17:03:19 BST
+% Author: Lucas G. S. Jeub
+% Email: jeub@maths.ox.ac.uk
 parseArgs=inputParser();
 parseArgs.KeepUnmatched=true;
 addParameter(parseArgs,'background_field',0);
@@ -30,10 +115,12 @@ end
 
 for i=1:n_layers
     ind=(1:layer_width)+(i-1)*layer_width;
-    
-    D(ind,ind)=distance_matrix(A{i});
-    C(ind,ind)=charge_matrix(groups{i});
-    
+    if options.distance_matrix
+        D(ind,ind)=A{i};
+    else
+        D(ind,ind)=distance_matrix(A{i});
+    end
+    C(ind,ind)=charge_matrix(groups{i},A{i});
     node_ind(ind)=1:layer_width;
 end
 
@@ -50,8 +137,12 @@ D=(D+D')/2;
         distances=all_shortest_paths(sparse(ii,jj,(max(vv)-vv+1),size(A,1),size(A,2)));
     end
 
-    function charges=charge_matrix(groups)
+    function charges=charge_matrix(groups, A)
         charges=1-(groups*groups')./(norm2(groups)*norm2(groups)'+eps);
+        background=ones(size(A));
+        background(A>0)=0;
+        charges=charges.*(options.community_field(:)*options.community_field(:)')+...
+        options.background_field(:)*options.background_field(:)'.*background;
     end
 
     function norm=norm2(v)
